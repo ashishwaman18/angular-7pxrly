8bf0be7ab2ceffe18ea765b1625f0c59ee352b13
ashishwaman18 <ashishwaman18@gmail.com>
2019-05-26 16:00:45 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  todoArray = [];
-  isinvalid = false;
+  todoArray=[];
+  isinvalid=false;
+  //storedarray=[];
 
-  constructor() {
+
+  ngOnInit(){
+
+    if (localStorage.getItem("services_assigned") === null) {
+      localStorage.setItem('services_assigned', JSON.stringify(this.todoArray)); 
+      
+    }
+
     this.todoArray = JSON.parse(localStorage.getItem('services_assigned'));
+   
   }
-  addTodo(toDo) {
 
-    if (toDo.value == '') {
-      this.isinvalid = true;
-      return false;
+      addTodo(toDo){
+
+    if(toDo.value==''){
+
+      this.isinvalid=true;
+      return false; 
     }
-    else {
-      this.isinvalid = false;
-      this.todoArray.push({
-        title: toDo.value,
-        isChecked: false
-      });
-      localStorage.setItem('services_assigned', JSON.stringify(this.todoArray));
-      toDo.value = null;
+    else{
+      this.isinvalid=false;
+        this.todoArray.push({
+          title: toDo.value,
+          isChecked: false
+        });
+
+        localStorage.setItem('services_assigned', JSON.stringify(this.todoArray)); 
+
+    // this.storedarray = JSON.parse(localStorage.getItem('services_assigned'));
+
+        toDo.value=null;
 
     }
-  }
+      }
+
+  
+
 
   deleteItem(i) {
-    this.todoArray.splice(i, 1);
-    localStorage.setItem('services_assigned', JSON.stringify(this.todoArray));
+    // const index: number = this.todoArray.indexOf(todo);
+    // if (index !== -1) {
+        this.todoArray.splice(i, 1);
+        localStorage.setItem('services_assigned', JSON.stringify(this.todoArray)); 
 
-  }
+    // }        
+}
 
-  alterCheck(todo) {
-    todo.isChecked = !todo.isChecked;
-  }
+
+  alterCheck(todo){  
+    todo.isChecked=!todo.isChecked;
+    }
 }
